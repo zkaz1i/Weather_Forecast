96f71f3f618c5a86d0ec21a9acc385caee6b9bdd
zkaz1i <kazizainab200@gmail.com>
2026-05-05 17:48:32 +0530

    remove health check endpoint from application

--- app.py
+++ app.py
@@ -474,10 +474,6 @@ def index():
         favorites_json=json.dumps(favorites),
     )
 
-@app.route("/health")
-def health():
-    return "OK"
-
 if __name__ == "__main__":
     import os
     port = int(os.environ.get("PORT",8080))
