1fe2d90d7957094cda3d607943e21c4748c734ba
zkaz1i <kazizainab200@gmail.com>
2026-05-05 18:03:21 +0530

    create database tables on application startup

--- app.py
+++ app.py
@@ -474,6 +474,9 @@ def index():
         favorites_json=json.dumps(favorites),
     )
 
+with app.app_context():
+    db.create_all()
+
 if __name__ == "__main__":
     import os
     port = int(os.environ.get("PORT",8080))
