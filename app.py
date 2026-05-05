f56ae16df45126f06611739f6e05b08096a02ce7
zkaz1i <kazizainab200@gmail.com>
2026-05-05 06:34:24 +0530

    update default port to 8080 in app run configuration

--- app.py
+++ app.py
@@ -476,5 +476,5 @@ def index():
 
 if __name__ == "__main__":
     import os
-    port = int(os.environ.get("PORT",5000))
+    port = int(os.environ.get("PORT",8080))
     app.run(host="0.0.0.0", port=port)
