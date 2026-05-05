a6383ec496e6770554693e39ffe479e6b71b67ac
zkaz1i <kazizainab200@gmail.com>
2026-05-05 22:07:36 +0530

    update SQLALCHEMY_DATABASE_URI to use relative path for database

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ print("App starts")
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY", "dev-secret-change-in-production")
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////tmp/weather.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///weather.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
